49eff5196a6aaf2bd8cf716ade678c8081a577d1
LivingtonMerello98 <livington.merello@gmail.com>
2024-04-23 17:43:25 +0200

    [UPD] fix (name ---> firstName)

--- script.js
+++ script.js
@@ -3,6 +3,6 @@
         const firstName = prompt( "Please enter your name:" );
         const lastName = prompt( "Please enter your last name:");
         const favouriteColor =  prompt("What is your favorite color?");
-        document.getElementById("my_id").innerHTML = name +lastName + favouriteColor + n;
+        document.getElementById("my_id").innerHTML = firstName +lastName + favouriteColor + n;
         // bonus1
         console.log(firstName + lastName + favouriteColor);
